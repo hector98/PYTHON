2a9bce29acd2da6dcfc62cb503e443a975a65157
hector98 <barrioshector13@gmail.com>
2019-10-04 10:55:23 -0500

    Agregar reto Python_If-Else aun incompleto por algunos detalles

---
+++ Python_If-Else.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+    if n % 2 == 1:
+
+    elif n>=2 and n<=5:
+        print("Not Weird")
+        print("Weird")
+
+else:
+    if n>=6 and n<=20:
+         print("Weird")
+    else:
+     if n>20:
+         print("weird")
+     else:
+         print("Not Weird")
