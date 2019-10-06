c854182a3a6ddd3204349e8cbde1e58f3e975bcd
hector98 <barrioshector13@gmail.com>
2019-10-06 17:33:00 -0500

    Terminar reto Python_If-Else

--- Python_If-Else.py
+++ Python_If-Else.py
@@ -12,16 +12,11 @@ if __name__ == '__main__':
 
     n = int(input().strip())
     if n % 2 == 1:
-
-    elif n>=2 and n<=5:
-        print("Not Weird")
         print("Weird")
-
-else:
-    if n>=6 and n<=20:
-         print("Weird")
     else:
-     if n>20:
-         print("weird")
-     else:
-         print("Not Weird")
+        if n>=2 and n<=5:
+            print ("Not Weird")
+        if n>=6 and n<=20:
+            print ("Weird")
+        if n>20:
+            print("Not Weird")
