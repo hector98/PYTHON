b59fa11150deaac5d328ed52074b9c7ee84c7dc8
hector98 <barrioshector13@gmail.com>
2019-10-11 21:05:13 -0500

    Agregar problemas Print_Function, String_Split_and_Join, What_s_Your_Name, Write_a_Function; ya resueltos

---
+++ String_Split_and_Join.py
@@ -0,0 +1,12 @@
+def split_and_join(line):
+    a = line
+
+    a = a.split(" ")
+    a = "-".join(a)
+
+    return a
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
